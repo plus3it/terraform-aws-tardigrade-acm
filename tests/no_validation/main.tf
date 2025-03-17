e8a06709d3c80b5897e7c7604bd9ba8851e8d7d5
Loren Gordon <lorengordon@users.noreply.github.com>
2025-03-17 15:02:19 -0400

    Creates zone during test to reproduce failure for simultaneous zone and acm creation

--- tests/no_validation/main.tf
+++ tests/no_validation/main.tf
@@ -1,8 +1,10 @@
 module "create_certificate" {
   source = "../../"
 
-  zone_id     = data.aws_route53_zone.this.zone_id
   domain_name = local.domain_name
+
+  zone_id   = aws_route53_zone.test.zone_id
+  zone_name = local.zone_name
 
   # Disable certificate validation due to the SAN "biz.cloudarmor.io", where the
   # zone is not the same as the zone of the zone_id, "tardigrade.cloudarmor.io"
@@ -13,15 +15,20 @@ module "create_certificate" {
     "foo.${local.domain_name}",
     "bar.${local.domain_name}",
     "baz.${local.domain_name}",
+    "abc.${local.zone_name}",
     "biz.cloudarmor.io",
   ]
+}
+
+resource "aws_route53_zone" "test" {
+  name = local.zone_name
 }
 
 locals {
   test_id = data.terraform_remote_state.prereq.outputs.random_string.result
 
-  domain_name = "${local.test_id}.test.${local.zone_name}"
-  zone_name   = "tardigrade.cloudarmor.io"
+  domain_name = "test.${local.zone_name}"
+  zone_name   = "${local.test_id}.tardigrade.cloudarmor.io"
 }
 
 data "terraform_remote_state" "prereq" {
@@ -29,11 +36,6 @@ data "terraform_remote_state" "prereq" {
   config = {
     path = "prereq/terraform.tfstate"
   }
-}
-
-data "aws_route53_zone" "this" {
-  name         = local.zone_name
-  private_zone = false
 }
 
 output "create_certificate" {
